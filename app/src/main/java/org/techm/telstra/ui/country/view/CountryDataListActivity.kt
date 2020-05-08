0876a1ef6ffb12b83df40b8a51e9e2abd109b07b
Ayan <ayan.sinha007@gmail.com>
2020-05-08 18:10:24 +0530

    Removing hard coded resource device pixel values to dimension values for layouts

--- app/src/main/java/org/techm/telstra/ui/country/view/CountryDataListActivity.kt
+++ app/src/main/java/org/techm/telstra/ui/country/view/CountryDataListActivity.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -18,6 +19,7 @@ import org.techm.telstra.data.model.CountryDataModel
 import org.techm.telstra.data.model.CountryDataItem
 import org.techm.telstra.data.network.APIHelper
 import org.techm.telstra.data.network.RetrofitBuilder
+import org.techm.telstra.databinding.ActivityCountryBinding
 import org.techm.telstra.ui.country.adapter.CountryDataListAdapter
 import org.techm.telstra.ui.country.viewmodel.CountryViewModel
 import org.techm.telstra.ui.factory.CountryFactory
@@ -35,25 +37,26 @@ class CountryDataListActivity : AppCompatActivity() {
     private lateinit var swipeRefreshLayout: SwipeRefreshLayout
     private lateinit var imageViewNoConnection: ImageView
     private var list: ArrayList<CountryDataItem>? = null
+    private lateinit var dataBinding: ActivityCountryBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_country)
+        dataBinding = DataBindingUtil.setContentView(this , R.layout.activity_country)
         setupUI()
         setUpViewModel()
         if (isConnection()) {
             setupAPICall()
         }else {
             toastShort(Constants.NO_CONNECTION)
-            recyclerView.visibility = View.VISIBLE
-            progressBar.visibility = View.GONE
-            imageViewNoConnection.visibility = View.VISIBLE
+            dataBinding.recyclerView.visibility = View.VISIBLE
+            dataBinding.progressBar.visibility = View.GONE
+            dataBinding.imageViewNoConnection.visibility = View.VISIBLE
         }
 
         /**
          * swipe on refresh
          */
-        swipeRefreshLayout.setOnRefreshListener {
+        dataBinding.swipeRefreshLayout.setOnRefreshListener {
             /**
              * if data-source is empty or no internet connection when application is on background
              */
@@ -64,25 +67,22 @@ class CountryDataListActivity : AppCompatActivity() {
                 retrieveCountryFeatureRows(list!!)
             }
             adapter.notifyDataSetChanged()
-            swipeRefreshLayout.isRefreshing = false
+            dataBinding.swipeRefreshLayout.isRefreshing = false
         }
     }
     /**
      * initialize ui elements
      */
     private fun setupUI() {
-        recyclerView = findViewById(R.id.recyclerView)
-        swipeRefreshLayout = findViewById(R.id.swipe_refresh_layout)
-        imageViewNoConnection = findViewById(R.id.imageViewNoConnection)
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        dataBinding.recyclerView.layoutManager = LinearLayoutManager(this)
         adapter = CountryDataListAdapter(arrayListOf() , this)
-        recyclerView.addItemDecoration(
+        dataBinding.recyclerView.addItemDecoration(
             DividerItemDecoration(
-                recyclerView.context,
-                (recyclerView.layoutManager as LinearLayoutManager).orientation
+                dataBinding.recyclerView.context,
+                (dataBinding.recyclerView.layoutManager as LinearLayoutManager).orientation
             )
         )
-        recyclerView.adapter = adapter
+        dataBinding.recyclerView.adapter = adapter
     }
     private fun setUpViewModel() {
         countryViewModel =
@@ -97,8 +97,8 @@ class CountryDataListActivity : AppCompatActivity() {
             it?.let {resource ->
                 when(resource.status) {
                     Status.SUCCESS -> {
-                        progressBar.visibility = View.GONE
-                        recyclerView.visibility = View.VISIBLE
+                        dataBinding.progressBar.visibility = View.GONE
+                        dataBinding.recyclerView.visibility = View.VISIBLE
                         resource.data?.let {it ->
                             supportActionBar?.title = it.body()?.title
                             if (list.isNullOrEmpty()) {
@@ -108,14 +108,14 @@ class CountryDataListActivity : AppCompatActivity() {
                         }
                     }
                     Status.ERROR -> {
-                        recyclerView.visibility = View.VISIBLE
-                        progressBar.visibility = View.GONE
-                        imageViewNoConnection.visibility = View.VISIBLE
-                        recyclerView.showSnackBar(Constants.ERROR_MSG)
+                        dataBinding.recyclerView.visibility = View.VISIBLE
+                        dataBinding.progressBar.visibility = View.GONE
+                        dataBinding.imageViewNoConnection.visibility = View.VISIBLE
+                        dataBinding.recyclerView.showSnackBar(Constants.ERROR_MSG)
                     }
                     Status.LOADING -> {
-                        progressBar.visibility = View.VISIBLE
-                        recyclerView.visibility = View.GONE
+                        dataBinding.progressBar.visibility = View.VISIBLE
+                        dataBinding.recyclerView.visibility = View.GONE
                     }
                 }
             }
@@ -127,6 +127,3 @@ class CountryDataListActivity : AppCompatActivity() {
         adapter.notifyDataSetChanged()
     }
 }
-
-
-
